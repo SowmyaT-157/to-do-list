d87482fd1b3577803d31881cc938ca39a8118ebc
sowmya <sowmya.t@everest.engineering>
2025-06-25 01:30:30 +0530

    Add a addTask functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 console.log("TO - Do App...")
+import { addTask,tasks}  from "./addTask/add";
+
+
+addTask("Buy milk");
+addTask("finish assignment");
+addTask("clean the room");
+
+
+tasks.forEach(task => {
+  console.log(`task: {
+  id: ${task.id};
+  description: "${task.description}";
+  Completed: ${task.completed};
+}`);
+});
