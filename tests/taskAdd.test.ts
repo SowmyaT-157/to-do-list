8657f1793685bac7daa3c7aa52b51ab3c07df399
sowmya <sowmya.t@everest.engineering>
2025-06-25 01:31:42 +0530

    Add unit test for addTask functionality

--- tests/taskAdd.test.ts
+++ tests/taskAdd.test.ts
@@ -1,6 +1,6 @@
-
-import {Task} from "../src/intefaces/data"
-
+import {  Task } from "../src/intefaces/data";
+import {addTask,tasks} from "../src/addTask/add";
+describe("addTask for Todo Management ", () => {
 beforeEach(() => {
   tasks.length = 0;      
 });
@@ -8,5 +8,8 @@ beforeEach(() => {
 test("addTask with description and autoincrement id", () => {
   const task1 = addTask("Buy milk");
   expect(task1).toEqual({ id: 1, description: "Buy milk", completed: false });
-
+  const task2 = addTask("complete assignment");
+  expect(task2).toEqual({ id: 2, description: "complete assignment", completed: false });
 });
+})
+
